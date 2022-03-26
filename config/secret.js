2248cecf8cc8f5d716be477825cd5b2908e86eb0
CSH <sayong4424@gmail.com>
2022-03-26 21:01:20 +0900

    220326 Feat :: 인증번호 전송 api 추가

--- config/secret.js
+++ config/secret.js
@@ -2,6 +2,10 @@
 // TODO: .gitignore에 추가하는거 앚지 마세요!
 module.exports = {
     'jwtsecret' :  '00INTR8SN0CoLL6eCt8IoNo6OtDcA6le88nD0are6Ig00Ht66',
+    'NCP_SENS_ACCESS' : 'CLWuUs7D6jVfjXsa44cg',
+    'NCP_SENS_SECRET' : 'DheJc13ywmAGSXLAsv0VkvLqzFXwpszjYK98SFHy',
+    'NCP_SENS_ID' : 'ncp:sms:kr:282806397335:collection',
+    'NCP_SENS_NUMBER' : '01038044424'
 };
 
 //INTRSN
