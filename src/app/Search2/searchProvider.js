7fd7db35183d9fbe35d52f37c10eceefbd7457df
Yurim Yang <yang980329@likelion.org>
2022-05-11 02:16:59 +0900

    220511 Fix :: delHistory 버그 수정6

--- src/app/Search2/searchProvider.js
+++ src/app/Search2/searchProvider.js
@@ -6,9 +6,9 @@ const searchDao = require("./searchDao");
 // Provider: Read 비즈니스 로직 처리
 
 // History 검색 내역검사 함수
-exports.historyCheck = async function (userIdx,PWWC,content,color) {
+exports.historyCheck = async function (userIdx,PWWC,content) {
   const connection = await pool.getConnection(async (conn) => conn);
-  const historyCheckResult = await searchDao.selectHistory(connection, userIdx,PWWC,content,color);
+  const historyCheckResult = await searchDao.selectHistory(connection, userIdx,PWWC,content);
   connection.release();
 
   return historyCheckResult;
