7a691d8eb71732f9edf1c8f6e8a566d733414c5c
Yurim Yang <yang980329@likelion.org>
2022-05-11 01:22:29 +0900

    220511 Fix :: delHistory 버그 수정1

--- src/app/Search2/searchDao.js
+++ src/app/Search2/searchDao.js
@@ -1,6 +1,8 @@
 
 // 새롭게 추가한 함수를 아래 부분에서 export 해줘야 외부의 Provider, Service 등에서 사용가능합니다.
 
+const nodemon = require("nodemon");
+
 // 검색 History 삭제하기 (개별) API
 
 async function updateHistoryEach(connection, userIdx, PWWC,content) {
@@ -34,18 +36,17 @@ async function updateHistoryAll(connection, userIdx, PWWC) {
 }
 
 // History 검색 내역검사 
-async function selectHistory(connection,userIdx,PWWC,conent) {
+async function selectHistory(connection,userIdx,PWWC,conent,color) {
   const selectHistoryQuery = `
     SELECT content
     FROM History
-    WHERE userIdx = ? AND PWWC = ? AND content = ?;`;
-  const IDRow = await connection.query(selectHistoryQuery, [userIdx,PWWC,conent]);
-  return IDRow;
+    WHERE userIdx = ? AND PWWC = ? AND content = ? AND color = ?;`;
+  const IDRow = await connection.query(selectHistoryQuery, [userIdx,PWWC,conent,color]);
+  console.log('IDRow:',IDRow);
+  console.log('IDRow[0]:',IDRow[0]);
+  console.log('[IDRow]:',[IDRow]);
+  return IDRow[0];
 }
-
-
-
-
 
 
 module.exports = {
