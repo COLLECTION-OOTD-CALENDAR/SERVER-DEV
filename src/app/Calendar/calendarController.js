acb034e923605556c100e3e20e6104535cabea3a
CSH <sayong4424@gmail.com>
2022-05-18 04:25:13 +0900

    220518 Refactor :: Calendar 주석 및 log, error 메세지 수정

--- src/app/Calendar/calendarController.js
+++ src/app/Calendar/calendarController.js
@@ -8,17 +8,16 @@ var lookpointPattern = /^[1-5]$/;
 /**
  * API No. 6
  * API Name : Monthly 달력 OOTD 부르기 API + JWT
- * [GET] /app/calendar/monthly/:userIdx
- * Path Variable : userIdx
+ * [GET] /app/calendar/monthly
  * jwt : userIdx
  */
 exports.calendarMonthly = async function (req, res) {
 
-    //console.log('[calendarController] getMonth start');
     const userIdx = req.verifiedToken.userIdx;
 
     const callMonthCalOotd = await calendarProvider.retrieveMonthlyList(userIdx);
     
+    // 형식 및 유효성 검사 - lookpoint pattern, date pattern
     for(i in callMonthCalOotd){
         if(!lookpointPattern.test(callMonthCalOotd[i].lookpoint)){
             return res.send(errResponse(baseResponse.LOOKPOINT_RESPONSE_ERROR));
@@ -31,7 +30,6 @@ exports.calendarMonthly = async function (req, res) {
     const monthCalFinalResult = {};
     monthCalFinalResult["monthly"] = callMonthCalOotd;
 
-    //console.log('[calendarController] getMonth finish')
     return res.send(response(baseResponse.SUCCESS_MONTHLY_CALENDAR, monthCalFinalResult));
 
 }
@@ -39,17 +37,16 @@ exports.calendarMonthly = async function (req, res) {
 /**
  * API No. 7
  * API Name : Weekly 달력 OOTD 부르기 API + JWT
- * [GET] /app/calendar/weekly/:userIdx
- * Path Variable : userIdx
+ * [GET] /app/calendar/weekly
  * jwt : userIdx
  */
 exports.calendarWeekly = async function (req, res) {
 
-    //console.log('[calendarController] getWeek start');
     const userIdx = req.verifiedToken.userIdx;
 
     const callWeekCalOotd = await calendarProvider.retrieveWeeklyList(userIdx);
 
+    // 형식 및 유효성 검사 - lookpoint pattern, date pattern, img
     for(i in callWeekCalOotd){
         if(!lookpointPattern.test(callWeekCalOotd[i].lookpoint)){
             return res.send(errResponse(baseResponse.LOOKPOINT_RESPONSE_ERROR));
@@ -66,7 +63,6 @@ exports.calendarWeekly = async function (req, res) {
     const weekCalFinalResult = {};
     weekCalFinalResult["weekly"] = callWeekCalOotd;
 
-    //console.log('[calendarController] getWeek finish')
     return res.send(response(baseResponse.SUCCESS_WEEKLY_CALENDAR, weekCalFinalResult));
 
 };
